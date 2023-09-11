dc1e6b695c68d7f980d080c5cf36920ea10b5b88
Franco Sebastian Muñoz <franco.mz92@hotmail.com>
2023-09-11 19:44:41 -0300

    Sube la clase 02 de JavaScript

--- JavaScript/e-commerce2022/client/js/index.js
+++ JavaScript/e-commerce2022/client/js/index.js
@@ -1,13 +1,37 @@
-const $shopContent = document.querySelector('#shopContent')
+const $shopContent = document.querySelector("#shopContent");
+const cart = [];
+
+const addToCart = product => {
+  const itemCartIndex = cart.findIndex(item => item.id == product.id);
+  if (itemCartIndex === -1) {
+    cart.push({
+      ...product,
+      quantity: 1
+    });
+  } else {
+    cart[itemCartIndex] = {
+      ...product,
+      quantity: cart[itemCartIndex].quantity + 1
+    };
+  }
+};
 
 products.forEach(product => {
-    const $product = document.createElement('div')
-    const $productName = document.createElement('h3')
-    const $productPrice = document.createElement('p')
-    const $productImage = document.createElement('img')
-    $productName.textContent = product.productName
-    $productPrice.textContent = `$${product.price}`
-    $productImage.setAttribute('src', product.img)
-    $product.append($productImage, $productName, $productPrice)
-    $shopContent.append($product)
-})
+  const $product = document.createElement("div");
+  const $productName = document.createElement("h3");
+  const $productPrice = document.createElement("p");
+  const $productImage = document.createElement("img");
+  const $productBuyButton = document.createElement("button");
+  $productName.textContent = product.productName;
+  $productPrice.textContent = `$${product.price}`;
+  $productImage.setAttribute("src", product.img);
+  $productBuyButton.textContent = "Comprar";
+
+  $product.append($productImage, $productName, $productPrice, $productBuyButton);
+  $shopContent.append($product);
+
+  $productBuyButton.addEventListener("click", event => {
+    event.preventDefault();
+    addToCart(product)
+  });
+});
