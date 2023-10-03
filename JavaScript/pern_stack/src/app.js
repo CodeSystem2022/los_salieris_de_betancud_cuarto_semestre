0404d54b5df9df5154c8bfde4e53bfbc8a9a248d
Franco Sebastian Muñoz <franco.mz92@hotmail.com>
2023-10-03 12:45:14 -0300

    Crea controllers y routers para tareas y auth

--- JavaScript/pern_stack/src/app.js
+++ JavaScript/pern_stack/src/app.js
@@ -1,11 +1,17 @@
 import express from "express";
 import morgan from "morgan";
 
+import tareasRouter from "./router/tareas.routes.js";
+import authRouter from "./router/auth.routes.js";
+
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+app.use("/api", tareasRouter)
+app.use("/api", authRouter)
 
 app.get("/", (req, res) => res.json({message: "Bienvenidos a mi proyecto"}));
 
